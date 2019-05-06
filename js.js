d10938acce53366887d9acd8fa5cbfc6b3f41146
JuliaVx <pet.yulija@yandex.ru>
2019-05-06 20:08:00 +0300

    Add jquery-framework

--- js.js
+++ js.js
@@ -1,8 +1,24 @@
-<script type="text/javascript">
- 	$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
-  var target = $(this).attr('href'),
-   bl_top = $(target).offset().top ;
-  $('body, html').animate({scrollTop:bl_top}, 1000);
-  return false;
- })
-</script>
+//<script type="text/javascript">
+ /*$('a[data-target="anchor"]').bind('click.smoothscroll', function(){
+ 	var target = $(this).attr('href'),
+	bl_top = $(target).offset().top ;
+  	$('body, html').animate({scrollTop:bl_top}, 1000);
+  	console.log('!!!!');
+  	return false;
+ })*/
+//</script>
+$(document).ready(function() {
+ 
+ 
+   $("a.scroll1").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+         duration: 1000,
+         easing: "swing"
+      });
+      return false;
+   });
+ 
+ 
+});
