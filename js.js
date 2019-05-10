f9c14416cf0593b1b6b0749c8eecfc112798f140
JuliaVx <pet.yulija@yandex.ru>
2019-05-10 21:22:07 +0300

    added new icons for footer

--- js.js
+++ js.js
@@ -1,24 +1,12 @@
-//<script type="text/javascript">
- /*$('a[data-target="anchor"]').bind('click.smoothscroll', function(){
- 	var target = $(this).attr('href'),
-	bl_top = $(target).offset().top ;
-  	$('body, html').animate({scrollTop:bl_top}, 1000);
-  	console.log('!!!!');
-  	return false;
- })*/
-//</script>
+
 $(document).ready(function() {
- 
- 
-   $("a.scroll1").click(function() {
-      $("html, body").animate({
-         scrollTop: $($(this).attr("href")).offset().top + "px"
-      }, {
+	$("a.scroll1").click(function() {
+      	$("html, body").animate({
+         	scrollTop: $($(this).attr("href")).offset().top + "px"
+     	 }, {
          duration: 1000,
          easing: "swing"
-      });
-      return false;
-   });
- 
- 
+      	});
+      	return false;
+   }); 
 });
